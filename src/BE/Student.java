7f02950c335de577c5fdc7286c9cf7cc1f979d2b
robie sun <robie.sun.rs@gmail.com>
2017-02-07 17:43:52 +0100

    3rd commit
    BE for student

---
+++ src/BE/Student.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BE;
+
+/**
+ * 
+ * @author robiesun
+ */
+public class Student {
+    
+    private String name;
+    private int Id;
+    private String email; // note for robie: ask teacher if string is appropriate for email(@ , .)?
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
--- src/GUI/Controller/MainScheduleController.java
+++ src/GUI/Controller/MainScheduleController.java
@@ -7,19 +7,59 @@ package GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
- * This class will show the schedule for whole semester.
+ * FXML Controller class
  *
  * @author robiesun
  */
 public class MainScheduleController implements Initializable {
 
-    
+    @FXML
+    private TextField textFieldName;
+    @FXML
+    private TextField textFieldID;
+    @FXML
+    private TextField textFieldEmail;
+    @FXML
+    private TableView<?> scheduleTableView;
+    @FXML
+    private TextField textFieldDate;
+    @FXML
+    private Button previousBtn;
+    @FXML
+    private Button nextBtn;
+    @FXML
+    private Button weekBtn;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void logOutBtnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void previousBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void nextBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void weekBtnAction(ActionEvent event) {
+    }
     
 }

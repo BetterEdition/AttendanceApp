9faa8e9adb07f8f691d649e1b588e08f4b808d34
robie sun <robie.sun.rs@gmail.com>
2017-02-07 12:42:13 +0100

    First commit

---
+++ src/GUI/Controller/LogInController.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author robiesun
+ */
+public class LogInController implements Initializable {
+
+    @FXML
+    private TextField studentId;
+    
+    @FXML
+    private Button logIn;
+    @FXML
+    private PasswordField passwordField;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButton(ActionEvent event) {
+        
+        String username = studentId.getText();
+        String pass = passwordField.getText();
+        
+        if(username.equals("123456")&& pass.equals("robiesun"));
+        {
+             Stage stage = null;
+            Parent root = null;
+
+            if (event.getSource() == logIn)
+            {
+
+                //get reference to the button's stage  
+                stage = (Stage) logIn.getScene().getWindow();
+
+                //load up OTHER FXML document
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/View/MainSchedule.fxml"));
+                try
+                {
+                    root = fxmlLoader.load();
+                } catch (IOException ex)
+                {
+                    Logger.getLogger(LogInController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                //create a new scene with root and set the stage
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+        }
+      
+    }
+    
+}
+}
---
+++ src/Main/MainClass.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The log in view that allow students to get access of their attendance.
+ * @author robiesun
+ */
+public class MainClass extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/LogInView.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Log-in");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
